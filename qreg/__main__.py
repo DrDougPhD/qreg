17338deb77832dece22dcfcc1796b939b2e73cfa
Doug McGeehan <doug@kmnr.org>
2021-10-15 09:42:32 -0600

    establish a toy example scaffolding

--- qreg/__main__.py
+++ qreg/__main__.py
@@ -5,5 +5,16 @@ def main():
     print('Hello world!')
 
 
+def toy(args):
+    print('A toy example!')
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Execute the `qreg` Python package.')
+    parser.add_argument('subcommand',
+                        nargs='?',
+                        default=toy,
+                        help='compute the quantile regression estimates on a toy example')
+
+    args = parser.parse_args()
+    args.subcommand(args)
